468fae5b222b17f14a2f5096e0fad80bc0152efb
Oliver Gugger <gugger@puzzle.ch>
2017-08-18 09:02:18 +0200

    Disable XML validation

--- src/main/java/ch/illubits/api/control/CollectorService.java
+++ src/main/java/ch/illubits/api/control/CollectorService.java
@@ -91,6 +91,14 @@ public class CollectorService {
 
     private void parseXML(List<Station> stations, List<Measurement> measurements) throws Exception {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        dbf.setValidating(false);
+        dbf.setNamespaceAware(true);
+        dbf.setFeature("http://xml.org/sax/features/namespaces", false);
+        dbf.setFeature("http://xml.org/sax/features/validation", false);
+        dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
+        dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document document = db.parse(XML_URL);
         document.getDocumentElement().normalize();
